d6a0f6c755468637bb00c03f23731ed717ccf1a1
Nickshutoff <153721339+Nickshutoff@users.noreply.github.com>
2024-04-06 22:35:34 +0300

    Delete styles/components/index.js

--- styles/components/index.js
+++
@@ -1,9 +0,0 @@
-let spoiler = document.querySelectorAll(".site");
-let activeIndex = null;
-console.log(spoiler)
-
-spoiler.forEach => (){
-   el.EventTarget.addEventListener("click", () => {
-    activeIn
-    })
-}
